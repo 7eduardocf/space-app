fada4239b9a0d0418c998f20fb4b6984716fa2f1
Eduardo Frota <eduardocfrota901@gmail.com>
2025-03-17 00:10:56 -0400

    Feita a criação da estrutura da sidebar

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components"
 import EstilosGlobais from "./components/global-styles/global-styles"
 import Cabecalho from "./components/Header/cabecalho"
+import BarraLateral from "./components/sidebar/barra-lateral"
 
 const FundoGradiente = styled.div`
     background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -14,6 +15,7 @@ function App() {
     <FundoGradiente>
         <EstilosGlobais/>
         <Cabecalho/>
+        <BarraLateral/>
     </ FundoGradiente>
   )
 }
