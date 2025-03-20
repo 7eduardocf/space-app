0f74bb07f15d6e8af89e8fdfe8232ec509e2b7e9
Eduardo Frota <eduardocfrota901@gmail.com>
2025-03-19 23:38:21 -0400

    Feita a criação dos componentes 'Populares' e 'Tags'

---
+++ space-app/src/components/Galeria/Populares/populares.jsx
@@ -0,0 +1,8 @@
+import Titulo from "../../titles/titulos"
+
+const Populares = () =>{
+    return(
+        <Titulo $alinhamento="center">Populares</Titulo>
+    )
+}
+export default Populares
--- space-app/src/components/Galeria/galeria.jsx
+++ space-app/src/components/Galeria/galeria.jsx
@@ -1,6 +1,27 @@
+import styled from "styled-components"
+import Titulo from "../titles/titulos"
+import Tags from "./Tags/tags"
+import Populares from "./Populares/populares"
+
+const GaleriaContainer = styled.div`
+    display: flex;
+`
+const SecaoFluida = styled.section`
+    flex-grow: 1;
+`
+
 const Galeria = () =>{
     return(
-        <h2>Galeria</h2>
+        <>
+            <Tags/>
+            <GaleriaContainer>
+                <SecaoFluida>
+                    <Titulo>Navegue pela galeria</Titulo>
+
+                </SecaoFluida>
+                <Populares/>
+            </GaleriaContainer>
+        </>
     )
 }
 
