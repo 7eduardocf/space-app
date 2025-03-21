dfdc514e155caae123f419472bf05c7fcd4b157f
Eduardo Frota <eduardocfrota901@gmail.com>
2025-03-20 23:44:19 -0400

    Feita a estilização do componente tags

--- space-app/src/components/Galeria/Tags/tags.jsx
+++ space-app/src/components/Galeria/Tags/tags.jsx
@@ -1,11 +1,39 @@
+import styled from "styled-components"
 import tags from "./tags.json"
+
+const TagTitulo = styled.h3`
+    color: #D9D9D9;
+    font-size: 24px;
+    margin: 0;
+`
+
+const Tag = styled.button`
+    font-size: 24px;
+    color: #FFFFFF;
+    background: rgba(217, 217, 217, 0.3);
+    border-radius: 10px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+    padding: 12px;
+    box-sizing: border-box;
+    border: 2px solid transparent;
+    &:hover {
+      border-color: #C98CF1;
+    }
+`
+const TagsContainer = styled.div`
+    display: flex;
+    margin-top: 20px;
+    gap: 16px;
+`
+
 
 const Tags = ()=>{
     return(
-        <>
+        <TagsContainer>
             <p>Busque por tags:</p>
-            {tags.map( tag => <button key={tag.id}>{tag.titulo}</button>)}
-        </>
+            {tags.map( tag => <Tag key={tag.id}> <TagTitulo>{tag.titulo}</TagTitulo></Tag>)}
+        </TagsContainer>
     )
 }
 export default Tags
